dd5ad590c64060f84a66ad9d41b5d175a3764007
MendozaServin <carimrafa@gmail.com>
2022-03-28 09:22:51 -0500

    Inicio del back

---
+++ src/main/java/a/lo/mexa/backend/dto/UsuarioDTO.java
@@ -0,0 +1,34 @@
+package a.lo.mexa.backend.dto;
+
+public class UsuarioDTO {
+    int id;
+    String nombre;
+    String Correo;
+
+    public UsuarioDTO() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+}
---
+++ src/main/java/a/lo/mexa/backend/model/UsuarioModel.java
@@ -0,0 +1,44 @@
+package a.lo.mexa.backend.model;
+
+public class UsuarioModel {
+
+    int id;
+    String nombre;
+    String correo;
+    String contraseña;
+
+    public UsuarioModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
