9e2444c8105e9ceedc2a4d552afed763f865fd51
MendozaServin <carimrafa@gmail.com>
2022-03-30 21:58:39 -0500

    La v...

--- src/main/java/a/lo/mexa/backend/controller/ApplicationController.java
+++ src/main/java/a/lo/mexa/backend/controller/ApplicationController.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/1")
+@RequestMapping(value = "/api/v1")
 public class ApplicationController {
 
     @Autowired
